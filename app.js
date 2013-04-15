d17d5bdebba9bdbe16d7c994b245daee4e275d50
Christian Nilsson <christian@klamby.com>
2013-04-15 13:29:07 +0200

    Moved configuration to own file

--- app.js
+++ app.js
@@ -4,11 +4,28 @@
 var express = require('express'),
 	app = express(),
 	controllers = require('./controllers').controllers,
+	config = require('./configure.json'),
 	server;
+
+// All environments
+app.configure(function () {
+	Object.keys(config).filter(function (c) { return c !== 'development' && c !== 'production'; })
+					   .forEach(function (c) { app.set(c, config[c]); });
+});
+
+// Development only
+app.configure('development', function () {
+	Object.keys(config.development).forEach(function (c) { app.set(c, config.development[c]); });
+});
+
+// Production only
+app.configure('production', function () {
+	Object.keys(config.production).forEach(function (c) { app.set(c, config.production[c]); });
+});
 
 // Controllers or routes
 controllers.forEach(function (c) { app[c.verb](c.route, c.fn); });
 
 // Start server
-server = app.listen(3000);
-console.log('Listening on port %d in %s mode...', 3000, app.get('env'));
+server = app.listen(app.get('port'));
+console.log('Listening on port %d in %s mode...', app.get('port'), app.get('env'));
