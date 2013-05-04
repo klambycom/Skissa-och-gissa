ab736cc0ce312f6ab51cdf1d6ac612923dcb56fa
Christian Nilsson <christian@klamby.com>
2013-05-05 00:09:40 +0200

    Added abbility to change room

--- websockets.js
+++ websockets.js
@@ -2,52 +2,75 @@
 'use strict';
 
 var Player = require('./lib/server/Player').Player,
-	players = [];
+	rooms = {};
 
 exports.listen = function (app) {
-	var io = require('socket.io').listen(app),
-		room = io.of('/room');
+	var io = require('socket.io').listen(app);
 
-	room.on('connection', function (user) {
-		var player;
+	io.sockets.on('connection', function (socket) {
+		// Player joins
+		socket.emit('identify-player', { id: socket.id });
 
-		// Player joins room
-		user.emit('identify-player', { id: user.id });
+		// Browser sends player data
+		socket.on('identify-player', function (data) {
+			// Store player in the socket session for this client
+			socket.player = new Player(data);
 
-		// Browser send player data
-		user.on('identify-player', function (data) {
-			// Create player with new data
-			player = new Player(data);
+			// Store room in the socket session for this client
+			socket.room = 'lobby';
 
-			// Remove player from array
-			players = players.filter(function (p) { return p.getSocketID() !== user.id; });
+			// Join lobby
+			socket.join('lobby');
 
-			// Add player to array
-			players.push(player);
-
-			// Welcome player
-			user.emit('server-message', { text: 'Välkommen ' + player.getFullName() + '!' });
+			// Store player in lobby
+			//rooms.lobby.push(socket.player);
 		});
 
-		user.on('update-player-data', function (data) {
-			room.emit('server-message', { text: data.name + ' har bytt namn.' });
-		});
+		// Join a game
+		socket.on('join-room', function (room) {
+			// Leave the current room (stored in session)
+			socket.leave(socket.room);
+			//rooms[socket.room] = rooms[socket.room].filter(function (p) { return p.getSocketID() !== socket.id; });
 
-		// Get drawing-points from player, and send to the others
-		user.on('canvas', function (data) {
-			//room.emit('canvas', data);
-			user.broadcast.emit('canvas', data);
-		});
+			// Send message to old room
+			socket.broadcast.to(socket.room).emit('server-message', { text: socket.player.getFullName() + ' har lämnat spelet.' });
 
-		// Player sends message
-		user.on('user-message', function (data) {
-			// Send the message to all player in room
-			room.emit('user-message', { text: data, player: player.getAllData() });
+			// Join new room
+			socket.join(room);
+			socket.room = room;
+			//rooms[room].push(socket.player);
+
+			// Echo to the room that a player has connected to their room
+			socket.broadcast.to(room).emit('server-message', { text: socket.player.getFullName() + ' har anslutet!' });
+
+			// TODO Echo to client some information about the room (who is drawing, when its my turn, etc.)
+			//socket.emit('server-message', { text: 'Välkommen ' + socket.player.getFullName() + '!' });
 		});
 
 		// Player disconnect
-		user.on('disconnect', function () {
-			room.emit('server-message', { text: player.getFullName() + ' har lämnat spelet.' });
+		socket.on('disconnect', function () {
+			console.log(socket.room);
+			socket.broadcast.to(socket.room).emit('server-message', { text: socket.player.getFullName() + ' har lämnat spelet.' });
+		});
+
+		// TODO
+		socket.on('update-player-data', function (data) {
+			socket.broadcast.to(socket.room).emit('server-message', { text: data.name + ' har bytt namn.' });
+		});
+
+
+
+
+		// Get drawing-points from player, and send to the others
+		socket.on('canvas', function (data) {
+			//room.emit('canvas', data);
+			socket.broadcast.to(socket.room).emit('canvas', data);
+		});
+
+		// Player sends message
+		socket.on('user-message', function (data) {
+			// Send the message to all player in room
+			socket.broadcast.to(socket.room).emit('user-message', { text: data, player: socket.player.getAllData() });
 		});
 	});
 
