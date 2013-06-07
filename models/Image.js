29d9e164311776ac9a37a717a4f243712312e5c6
Christian Nilsson <christian@klamby.com>
2013-06-07 14:46:10 +0200

    Model for images

---
+++ models/Image.js
@@ -0,0 +1,40 @@
+/*jslint node: true, es5: true, nomen: true */
+'use strict';
+
+var mongoose = require('mongoose'),
+	graph = require('fbgraph'),
+	imageSchema;
+
+/*
+ * Schema
+ */
+imageSchema = mongoose.Schema({
+	word: String,
+	url: String,
+	player: { type: String, default: 'unknown' },
+	created_at: { type: Date, default: Date.now }
+});
+
+/*
+ * Statics
+ */
+imageSchema.statics.latest = function (p, cb) {
+	this.model('ImageModel').findOne({ player: p }).sort({ created_at: -1 }).exec(cb);
+};
+
+imageSchema.statics.findImage = function (id, cb) {
+	this.model('ImageModel').findOne({ _id: id }).exec(function (err, data) {
+		graph.get(data.player + '?fields=name,link,picture', function (fberr, fb) {
+			data.name = fb.name;
+			data.link = fb.link;
+			data.picture = fb.picture.data.url;
+			cb(err, fberr, data);
+		});
+	});
+};
+
+//ImageModel.findOne({ _id: req.params.id }, function (err, data) {
+
+//ImageModel.findOne({ player: req.params.player }).sort({ created_at: -1 }).limit(1).exec(function (err, data) {
+
+module.exports = mongoose.model('ImageModel', imageSchema);
