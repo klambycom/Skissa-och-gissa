fcd4ffc98e4d75bb830737f52b7c957d15207f01
Christian Nilsson <christian@klamby.com>
2013-06-07 14:50:57 +0200

    More info about drawing

--- public/templates/donemessage.js
+++ public/templates/donemessage.js
@@ -3,22 +3,42 @@
 templates['donemessage.hbs'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [3,'>= 1.0.0-rc.4'];
 helpers = helpers || Handlebars.helpers; data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
-
-  buffer += "<div class=\"drawing-done\">\n  <div class=\"word\">";
-  if (stack1 = helpers.word) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = depth0.word; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n  <a href=\"/p/";
+  if (stack1 = helpers.fid) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.fid; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</div>\n  <a href=\"";
-  if (stack1 = helpers.img) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = depth0.img; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+    + "/latest\" target=\"_blank\">\n  ";
+  return buffer;
+  }
+
+function program3(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n  <a href=\"";
+  if (stack1 = helpers.url) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.url; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
     + "\" download=\"";
   if (stack1 = helpers.word) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.word; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + ".png\">\n    <img src=\"";
+    + ".png\" target=\"_blank\">\n  ";
+  return buffer;
+  }
+
+  buffer += "<div class=\"drawing-done\">\n  <div class=\"word\">";
+  if (stack1 = helpers.word) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.word; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</div>\n  ";
+  stack1 = helpers['if'].call(depth0, depth0.fid, {hash:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n    <img src=\"";
   if (stack1 = helpers.img) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.img; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
@@ -26,7 +46,11 @@ helpers = helpers || Handlebars.helpers; data = data || {};
   if (stack1 = helpers.word) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.word; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\" style=\"width: 100%\">\n  </a>\n  <img src=\"gfx/nopic50.png\" alt=\"\" class=\"user\" />\n  <div class=\"by\">";
+    + "\" style=\"width: 100%\">\n  </a>\n  <img src=\"";
+  if (stack1 = helpers.picture) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.picture; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" alt=\"\" class=\"user\" />\n  <div class=\"by\">";
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
