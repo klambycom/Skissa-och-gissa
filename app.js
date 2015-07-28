d7ce6074b021305b5bebdc8208fd32673d24830d
klambycom <christian@klamby.com>
2015-07-28 22:17:44 +0200

    Production db

--- app.js
+++ app.js
@@ -41,7 +41,17 @@ Object
   }
 
 // Configure production only
-if (app.get('env') === 'production') { setAll(config.production); }
+if (app.get('env') === 'production') {
+  setAll(config.production);
+
+  // OpenShift MongoDB
+  var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    process.env.OPENSHIFT_APP_NAME;
+  app.set('db', connection_string);
+}
 
 // MongoDB
 mongoose.connect(app.get('db'));
