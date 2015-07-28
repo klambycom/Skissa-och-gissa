f89c6cb3dc7e892eb2b1bec039bb58dcbc479347
klambycom <christian@klamby.com>
2015-07-28 22:47:42 +0200

    Change ports

--- app.js
+++ app.js
@@ -24,34 +24,36 @@ Object
   .filter(function (c) { return c !== 'development' && c !== 'production'; })
   .forEach(set(config));
 
-  app.use(express['static'](app.get('public folder') || 'public'));
-  //app.use(express.bodyParser());
-  //app.use(express.cookieParser(app.get('cookie secret')));
-  app.use(flash());
+app.use(express['static'](app.get('public folder') || 'public'));
+//app.use(express.bodyParser());
+//app.use(express.cookieParser(app.get('cookie secret')));
+app.use(flash());
 
-  // Configure development only
-  if (app.get('env') === 'development') {
-    setAll(config.development);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT);
 
-    app.use(function (req, res, next) {
-      // Print information about request to console
-      console.log('%s %s', req.method, req.url);
-      next();
-    });
-  }
+// OpenShift MongoDB
+var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+app.set('db', connection_string);
 
-// Configure production only
-if (app.get('env') === 'production') {
-  setAll(config.production);
-
-  // OpenShift MongoDB
-  var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-    process.env.OPENSHIFT_APP_NAME;
-  app.set('db', connection_string);
-}
+//// Configure development only
+//if (app.get('env') === 'development') {
+//  setAll(config.development);
+//
+//  app.use(function (req, res, next) {
+//    // Print information about request to console
+//    console.log('%s %s', req.method, req.url);
+//    next();
+//  });
+//}
+//
+//// Configure production only
+//if (app.get('env') === 'production') {
+//  //setAll(config.production);
+//}
 
 // MongoDB
 mongoose.connect(app.get('db'));
