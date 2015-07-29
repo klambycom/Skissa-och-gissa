90b4b3cf8b455ffd0c3e3fb7e69a3b1ca3133912
klambycom <christian@klamby.com>
2015-07-29 19:56:53 +0200

    Change configuration

--- app.js
+++ app.js
@@ -3,58 +3,19 @@
 
 require('./lib/utils/functional');
 
-var express = require('express'),
-    http = require('http'),
-    app = express(),
+var http = require('http'),
+    app = require('express')(),
     controllers = require('./controllers').controllers,
-    config = require('./configure.json'),
-    flash = require('connect-flash'),
     server,
     game = require('./lib/server/game').game,
     mongoose = require('mongoose'),
-    dict = require('./dictionary.json');
+    dict = require('./dictionary.json'),
+    config = require('./config');
 
 // Configure
-var set = function (obj) { return function (s) { app.set(s, obj[s]); }; },
-    setAll = function (obj) { Object.keys(obj).forEach(set(obj)); };
-
-// Configure all environments
-Object
-  .keys(config)
-  .filter(function (c) { return c !== 'development' && c !== 'production'; })
-  .forEach(set(config));
-
-app.use(express['static'](app.get('public folder') || 'public'));
-//app.use(express.bodyParser());
-//app.use(express.cookieParser(app.get('cookie secret')));
-app.use(flash());
-
-app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP);
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT);
-
-// OpenShift MongoDB
-var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
-app.set('db', connection_string);
-
-//// Configure development only
-//if (app.get('env') === 'development') {
-//  setAll(config.development);
-//
-//  app.use(function (req, res, next) {
-//    // Print information about request to console
-//    console.log('%s %s', req.method, req.url);
-//    next();
-//  });
-//}
-//
-//// Configure production only
-//if (app.get('env') === 'production') {
-//  //setAll(config.production);
-//}
+if (app.get('env') === 'development') { config.development(app); }
+if (app.get('env') === 'production') { config.production(app); }
+config.all(app);
 
 // MongoDB
 mongoose.connect(app.get('db'));
---
+++ config.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var flash = require('connect-flash');
+
+module.exports = {
+  all: function (app) {
+    app.set('title', 'Skissa och gissa');
+    app.set('view engine', 'hbs');
+
+    app.use(express['static'](app.get('public folder') || 'public'));
+    //app.use(express.bodyParser());
+    //app.use(express.cookieParser(app.get('cookie secret')));
+    app.use(flash());
+  },
+
+  development: function (app) {
+    app.set('ipaddr', 'localhost');
+    app.set('port', 3000);
+    app.set('db', 'mongodb://localhost/skissa-och-gissa');
+
+    app.use(function (req, res, next) {
+      // Print information about request to console
+      console.log('%s %s', req.method, req.url);
+      next();
+    });
+  },
+
+  production: function (app) {
+    app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP);
+    app.set('port', process.env.OPENSHIFT_NODEJS_PORT);
+
+    // OpenShift MongoDB
+    var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+      process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+      process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+      process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+      process.env.OPENSHIFT_APP_NAME;
+    app.set('db', connection_string);
+  }
+};
