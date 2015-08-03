330320b939634822d923a26ba83a383921867d28
klambycom <christian@klamby.com>
2015-08-03 18:19:15 +0200

    Setup jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
+var jshint = require('gulp-jshint');
 
 var paths = {
   js:   'src/react/**/*.js',
   node: 'src/server/**/*.js',
   test: 'test/**/*_test.js'
 };
+
+/*
+ * Testing
+ */
+
+gulp.task('lint', function () {
+  return gulp.src([paths.js, paths.node])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
 
 gulp.task('jasmine', function () {
   return gulp.src(paths.test)
@@ -16,4 +28,4 @@ gulp.task('watch:test', function () {
   gulp.watch([paths.js, paths.node, paths.test], ['jasmine']);
 });
 
-gulp.task('test', ['jasmine']);
+gulp.task('test', ['jasmine', 'lint']);
