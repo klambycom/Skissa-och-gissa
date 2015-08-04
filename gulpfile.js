2f6881436fc2019d3ab00ad1b574aa5aab4b3151
klambycom <christian@klamby.com>
2015-08-04 22:38:04 +0200

    Setup uglify

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var markdox = require('gulp-markdox');
 var rename = require('gulp-rename');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 var paths = {
   js:   'src/react/**/*.js',
@@ -23,6 +24,13 @@ gulp.task('browserify', function () {
   return gulp.src(paths.main)
     .pipe(browserify({ transform: 'reactify', debug: true }))
     .pipe(concat('main.js'))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('uglify', ['browserify'], function () {
+  return gulp.src('dist/main.js')
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest(paths.dist));
 });
 
