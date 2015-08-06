d4cc0d9f36ee2ca97ef8289017b77709aa10af89
klambycom <christian@klamby.com>
2015-08-06 21:01:58 +0200

    Remove some console.log

--- src/passport/init.js
+++ src/passport/init.js
@@ -3,13 +3,11 @@ var User = require('../server/db').User;
 
 module.exports = function (passport) {
   passport.serializeUser(function (user, done) {
-    console.log('serializing user:', user);
     done(null, user._id);
   });
 
   passport.deserializeUser(function (id, done) {
     User.findById(id, function (err, user) {
-      console.log('deserializing user:', user);
       // TODO Handle errors
       done(null, user);
     });
