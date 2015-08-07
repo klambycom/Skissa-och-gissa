6a7311a435dc8354ee53ca4522f5a6474eabe8a5
klambycom <christian@klamby.com>
2015-08-08 00:41:45 +0200

    Logout api

--- src/server/api.js
+++ src/server/api.js
@@ -1,7 +1,13 @@
-var api = require('express')();
+var api = require('express').Router();
 
 api.get('/', function (req, res) {
   res.send('API root');
+});
+
+// TODO Change to post
+api.get('/logout', function (req, res) {
+  req.logout();
+  res.status(204).send();
 });
 
 module.exports = api;
