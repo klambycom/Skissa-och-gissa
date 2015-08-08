c0b697895e41e75fec78731becd4d3a316e0822c
klambycom <christian@klamby.com>
2015-08-08 13:08:23 +0200

    Add games.js

---
+++ docs/node/games.markdown
@@ -0,0 +1,6 @@
+
+
+<!-- Start src/server/games.js -->
+
+<!-- End src/server/games.js -->
+
---
+++ src/server/games.js
@@ -0,0 +1,16 @@
+// join(player, room[, oldRoom]) // Eller så ska man inte kunna lämna lobbyn,
+// men det behöver nog ändå se ut så här?
+
+// leave(player) // Ta bort spelare när den disconnectar
+
+// players(room) // Returnera alla spelare i rummet/spelet
+
+// all() // Returnera alla rum/spel
+
+// get(room) // Returnera rum/spel (Skapa game-class och player-class)
+
+// on() // New room, remove room, add player to room, remove player to room, new room image
+
+
+
+
