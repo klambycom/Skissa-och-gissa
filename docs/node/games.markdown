d2ab5eb7ae80fb9cccf5274a4bb785c42e15f0f7
klambycom <christian@klamby.com>
2015-08-09 20:20:57 +0200

    More documentation

--- docs/node/games.markdown
+++ docs/node/games.markdown
@@ -2,31 +2,47 @@
 
 <!-- Start src/server/games.js -->
 
-## exports
-
 # Games
 
 Handle games, and let users join and leave games.
 
-## join()
+## join(playerId, roomId, oldRoomId)
 
-Join a room, and leave a old room if specified
+Join a room and leave the old room
 
-## leave()
+### Params:
+
+* **string** *playerId* - ID of the player
+* **string** *roomId* - ID of the new room
+* **string** *oldRoomId* - ID of the old room
+
+## leave(playerId)
 
 Leave room (and disconnect)
 
-## players()
+### Params:
+
+* **string** *playerId* - ID of the player
+
+## players(roomId)
 
 All players in a specific room
+
+### Params:
+
+* **string** *roomId* - ID of the room
 
 ## all()
 
 All players in all rooms
 
-## get()
+## get(roomId)
 
 Get room with roomId
+
+### Params:
+
+* **string** *roomId* - ID of the room
 
 ## get()
 
--- src/server/games.js
+++ src/server/games.js
@@ -6,39 +6,46 @@
  * @module games
  */
 
+/*!  */
+
 module.exports = {
 
   /**
-   * Join a room, and leave a old room if specified
+   * Join a room and leave the old room
    *
    * @function join
+   * @param {string} playerId - ID of the player
+   * @param {string} roomId - ID of the new room
+   * @param {string} oldRoomId - ID of the old room
    *
    * @fires player_added
    * @fires player_removed
    */
 
-  join: function (player, room, oldRoom) {
+  join: function (playerId, roomId, oldRoomId) {
   },
 
   /**
    * Leave room (and disconnect)
    *
    * @function leave
+   * @param {string} playerId - ID of the player
    *
    * @fires player_added
    * @fires player_removed
    */
 
-  leave: function (player) {
+  leave: function (playerId) {
   },
 
   /**
    * All players in a specific room
    *
    * @function players
+   * @param {string} roomId - ID of the room
    */
 
-  players: function (room) {
+  players: function (roomId) {
   },
 
   /**
@@ -54,6 +61,7 @@ module.exports = {
    * Get room with roomId
    *
    * @function get
+   * @param {string} roomId - ID of the room
    */
 
   get: function (roomId) {
