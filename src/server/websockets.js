b12d83bde83ac1a5cfb5286d0aa52eeae384020c
klambycom <christian@klamby.com>
2015-08-15 23:16:01 +0200

    Start on websocket for when user joins a room

--- src/server/websockets.js
+++ src/server/websockets.js
@@ -7,6 +7,24 @@ module.exports = function (app, socketio) {
   var io = socketio(app);
 
   io.on('connection', function (socket) {
+    // Create player from socket and join lobby
     var player = games.createPlayer(socket);
+
+    // TODO Join lobby!
+
+    // Player joins a room
+    // join-room data = { roomId: UUID }; // Client to server
+    // leave-room data = { playerId: UUID }; // Server to client
+    socket.on('join-room', function (data) {
+      // Tell socket.io to leave old room and join the new room
+      socket.leave(player.room.id);
+      socket.join(data.roomId);
+
+      // Tell the old room that the player have left the room
+      socket.broadcast.to(player.room.id).emit('leave-room', { playerId: player.uuid });
+
+      // Player joins the new room
+      games.join(player, data.roomId);
+    });
   });
 };
--- test/server/websockets_test.js
+++ test/server/websockets_test.js
@@ -7,7 +7,43 @@ chai.use(sinonChai);
 var expect = chai.expect;
 var websockets = rewire('../../src/server/websockets');
 
+// socket.on
+var on = (function () {
+  var events = {};
+
+  return {
+    add: function (event, listener) { events[event] = listener; },
+    call: function (event, data) { events[event](data); },
+    clear: function () { events = {}; }
+  };
+}());
+
+var runWebsockets = function (socket) {
+  // Call io.on('conneciton', function (socket) { ... }) with mocked socket
+  var onMock = function (event, cb) { if (event === 'connection') { cb(socket); } };
+  websockets(null, function () { return { on: onMock }; });
+  return on;
+};
+
 describe('Websockets', function () {
+
+  beforeEach(function () {
+    this.broadcastMock = { to: function () { return this; }, emit: function () { return this; } };
+    this.socketMock = {
+      id: 123,
+      on: on.add,
+      broadcast: this.broadcastMock,
+      leave: function () {},
+      join: function () {}
+    };
+    // Mock games.js
+    this.gamesMock = { createPlayer: function () {}, join: function () {} };
+    websockets.__set__('games', this.gamesMock);
+  });
+
+  afterEach(function () {
+    on.clear();
+  });
 
   it('should start socket.io with the first param as argument', function () {
     var socket = sinon.stub().returns({ on: function () {} });
@@ -22,15 +58,56 @@ describe('Websockets', function () {
   });
 
   it('should create a player when a user connects', function () {
-    // Mock games.js
-    var gamesMock = { createPlayer: sinon.spy() };
-    websockets.__set__('games', gamesMock);
-    // Call io.on('conneciton', function (socket) { ... }) with mocked socket
-    var socketMock = { id: 123 };
-    var onMock = function (event, cb) { if (event === 'connection') { cb(socketMock); } };
-    websockets(null, function () { return { on: onMock }; });
+    this.gamesMock.createPlayer = sinon.spy();
+    runWebsockets(this.socketMock);
 
-    expect(gamesMock.createPlayer).to.have.been.calledWith(socketMock);
+    expect(this.gamesMock.createPlayer).to.have.been.calledWith(this.socketMock);
+  });
+
+  describe('Event: join-room', function () {
+
+    beforeEach(function () {
+      this.player = { uuid: 'player1', room: { id: 'lobby' } };
+      this.gamesMock.createPlayer = sinon.stub().returns(this.player);
+      this.data = { roomId: '12345' }; 
+    });
+
+    it('should listen to "join-room" events', function () {
+      this.socketMock.on = sinon.spy();
+      runWebsockets(this.socketMock);
+
+      expect(this.socketMock.on).to.have.been.calledWith('join-room');
+    });
+
+    it('should call games.join with the new room id', function () {
+      this.gamesMock.join = sinon.spy();
+      runWebsockets(this.socketMock).call('join-room', this.data);
+
+      expect(this.gamesMock.join).to.have.been.calledWith(this.player, '12345');
+    });
+
+    it('should tell the old room that the player have left the room', function () {
+      this.broadcastMock.to = sinon.stub().returns(this.broadcastMock);
+      this.broadcastMock.emit = sinon.spy();
+      runWebsockets(this.socketMock).call('join-room', this.data);
+
+      expect(this.broadcastMock.to).to.have.been.calledWith(this.player.room.id);
+      expect(this.broadcastMock.emit).to.have.been.calledWith('leave-room', { playerId: this.player.uuid });
+    });
+
+    it('should tell socket.io to leave the old room', function () {
+      this.socketMock.leave = sinon.spy();
+      runWebsockets(this.socketMock).call('join-room', this.data);
+
+      expect(this.socketMock.leave).to.have.been.calledWith(this.player.room.id);
+    });
+
+    it('should tell socket.io to join the new room', function () {
+      this.socketMock.join = sinon.spy();
+      runWebsockets(this.socketMock).call('join-room', this.data);
+
+      expect(this.socketMock.join).to.have.been.calledWith(this.data.roomId);
+    });
   });
 
   //it('should send a join-event when a new user connects', function () {
