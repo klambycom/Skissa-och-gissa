cb93e91edeb3a94bcbad73fdecac369fef30dd4c
klambycom <christian@klamby.com>
2015-08-31 18:44:41 +0200

    Connect popup and flux

--- src/browser/lobby.js
+++ src/browser/lobby.js
@@ -1,7 +1,8 @@
 var Reflux = require('reflux');
 
 var actions = Reflux.createActions([
-    'flash' // Flash-messages
+    'flash', // Flash-messages
+    'alert' // PopUp-dialog
 ]);
 
 var store = Reflux.createStore({
@@ -9,8 +10,29 @@ var store = Reflux.createStore({
 
   onFlash: function (message) {
     this.trigger({ event: 'flash', type: '', data: message });
+  },
+
+  onAlert: function (message, title, extra, closeable) {
+    if (typeof title === 'undefined' || title === null) { title = 'Hej!'; }
+    if (typeof extra === 'undefined' || extra === null) { extra = ''; }
+    if (typeof closeable === 'undefined' || closeable === null) { closeable = true; }
+
+    this.trigger({
+      event: 'alert',
+      type: '',
+      data: {
+        message: message,
+        title: title,
+        extra: extra,
+        closeable: closeable
+      }
+    });
   }
 });
+
+global._alert = function (message, title, extra) {
+  actions.alert(message, title, extra);
+};
 
 module.exports = actions;
 module.exports.store = store;
