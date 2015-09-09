ea1bd5bfa9ed8319309e4987b90e35179b9a848b
klambycom <christian@klamby.com>
2015-09-09 23:36:12 +0200

    Start moving canvas to a react component

--- docs/js/components/game/game_plan/drawing_area.markdown
+++ docs/js/components/game/game_plan/drawing_area.markdown
@@ -2,7 +2,79 @@
 
 <!-- Start src/components/game/game_plan/drawing_area.js -->
 
-## React
+## Point
+
+Represents a Point.
+
+### Params:
+
+* **** *x* {int} The x cordination.
+* **** *y* {int} The y cordination.
+* **** *dragging* {boolean} True if not the first point.
+* **** *color* {string} The color of the point.
+* **** *size* {int} The size of the point.
+
+## data()
+
+Get all data about the point.
+
+### Return:
+
+* **Object** Returns a object with all data (x, y, color, size, dragging).
+
+## add(x, y, dragging)
+
+Add a new Point to the drawing.
+
+### Params:
+
+* **** *x* {int} The x cordination.
+* **** *y* {int} The y cordination.
+* **** *dragging* {boolean} True if its not the first point in current line.
+
+### Return:
+
+* **** Returns the newly created Point.
+
+## setColor(c)
+
+Set the color. All points after the color is changed till its
+changed again will have the same color.
+
+### Params:
+
+* **** *c* {string} The new color.
+
+## setSize(s)
+
+Set the size. All points after the size is changed til its changed
+again will have the same size.
+
+### Params:
+
+* **** *s* {int} The new size.
+
+## clear()
+
+Remove all points.
+
+## each(fn)
+
+Iterate through each point.
+
+### Params:
+
+* **** *fn* {function} Callback function that takes two parameters                      (previous and current). Previous is undefined
+                     if its the first point.
+
+## last(fn)
+
+Gets the previous and the current point.
+
+### Params:
+
+* **** *fn* {function} Callbakc function that takes two parameters                      (previous and current). Previous is undefined
+                     if its the first point.
 
 <!-- End src/components/game/game_plan/drawing_area.js -->
 
--- src/components/game/game_plan/drawing_area.js
+++ src/components/game/game_plan/drawing_area.js
@@ -1,10 +1,242 @@
 var React = require('react');
 
+/**
+ * Represents a Point.
+ *
+ * @class Point
+ * @constructor
+ * @param x {int} The x cordination.
+ * @param y {int} The y cordination.
+ * @param dragging {boolean} True if not the first point.
+ * @param color {string} The color of the point.
+ * @param size {int} The size of the point.
+ */
+var Point = function (x, y, dragging, color, size) {
+  this.getX = function () { return x; };
+  this.getY = function () { return y; };
+  this.getColor = function () { return color; };
+  this.getSize = function () { return size; };
+  this.getDragging = function () { return dragging; };
+};
+
+/**
+ * Get all data about the point.
+ *
+ * @method data
+ * @return {Object} Returns a object with all data (x, y, color, size, dragging).
+ */
+
+Point.prototype.data = function () {
+  return {
+    x: this.getX(),
+    y: this.getY(),
+    color: this.getColor(),
+    size: this.getSize(),
+    dragging: this.getDragging()
+  }
+};
+
+var points = (function () {
+  var color = '#df4b26';
+  var size = 5;
+  var points = [];
+  var forEach = function (fn, a) { a.forEach(fn); };
+
+  var add = function (x, y, dragging) {
+    var p;
+    if (x instanceof Object) {
+      p = new Point(x.x, x.y, !!x.dragging, x.color, x.size);
+    } else {
+      p = new Point(x, y, !!dragging, color, size);
+    }
+    points.push(p);
+    return p;
+  };
+
+  return {
+		/**
+		 * Add a new Point to the drawing.
+		 *
+		 * @method add
+		 * @param x {int} The x cordination.
+		 * @param y {int} The y cordination.
+		 * @param dragging {boolean} True if its not the first point in current line.
+		 * @return Returns the newly created Point.
+		 */
+
+		add: add,
+
+		addArray: forEach.bind(null, add),
+
+		nrOfPoints: function () { return points.length; },
+
+		/**
+		 * Set the color. All points after the color is changed till its
+		 * changed again will have the same color.
+		 *
+		 * @method setColor
+		 * @param c {string} The new color.
+		 */
+
+		setColor: function (c) { color = c; },
+
+		/**
+		 * Set the size. All points after the size is changed til its changed
+		 * again will have the same size.
+		 *
+		 * @method setSize
+		 * @param s {int} The new size.
+		 */
+
+		setSize: function (s) { size = s; },
+
+		/**
+		 * Remove all points.
+		 *
+		 * @method clear
+		 */
+
+		clear: function () { points = []; },
+
+		/**
+		 * Iterate through each point.
+		 *
+		 * @method each
+		 * @param fn {function} Callback function that takes two parameters
+		 *                      (previous and current). Previous is undefined
+		 *                      if its the first point.
+		 */
+
+		each: function (fn) {
+			var i, previous, current;
+			for (i = 0; i < points.length; i += 1) {
+				previous = points[i - 1] && points[i - 1].data();
+				current = points[i].data();
+				fn(previous, current);
+			}
+		},
+
+		/**
+		 * Gets the previous and the current point.
+		 *
+		 * @method last
+		 * @param fn {function} Callbakc function that takes two parameters
+		 *                      (previous and current). Previous is undefined
+		 *                      if its the first point.
+		 */
+
+		last: function (fn) {
+			var i = points.length - 1,
+				previous = points[i - 1] && points[i - 1].data(),
+				current = points[i].data();
+			fn(previous, current);
+		}
+  };
+}());
+
 module.exports = React.createClass({
+  getInitialState: function () {
+    return { painting: false, offsetX: 0, offsetY: 0 };
+  },
+
+  componentDidMount: function () {
+    this.context = this.refs.canvas.getDOMNode().getContext('2d');
+
+    // Prevent Chrome from selecting the canvas
+    // TODO Set on document? No need to select anything?
+    this.refs.canvas.getDOMNode().onselectstart = function () { return false; };
+    this.refs.canvas.getDOMNode().onmousedown = function () { return false; };
+
+    // Offset if needed
+    var opt = {}; // TODO
+    this.setState({
+      offsetX: opt.x || 0,
+      offsetY: opt.y || 0
+    });
+
+    this._clear();
+
+    this._start(); // TODO Remove!
+  },
+
+  _start: function () {
+    this.context.canvas.addEventListener('mousedown', this._startDrawing);
+    this.context.canvas.addEventListener('mouseup', this._stopDrawing);
+    this.context.canvas.addEventListener('mousemove', this._drawing);
+    this.context.canvas.addEventListener('mouseleave', this._stopDrawing); // Firefox etc.
+    this.context.canvas.addEventListener('mouseout', this._stopDrawing); // Chrome
+  },
+
+  _stop: function () {
+    this.context.canvas.removeEventListener('mousedown', this._startDrawing);
+    this.context.canvas.removeEventListener('mouseup', this._stopDrawing);
+    this.context.canvas.removeEventListener('mousemove', this._drawing);
+    this.context.canvas.removeEventListener('mouseleave', this._stopDrawing); // Firefox etc.
+    this.context.canvas.removeEventListener('mouseout', this._stopDrawing); // Chrome
+  },
+
+  _clear: function () {
+    this.setState({ painting: false });
+    // Remove all points
+    points.clear();
+    // Clear the canvas
+    this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+    // Pencil style
+    this.context.lineJoin = 'round';
+  },
+
+  _addPoint: function (x, y, dragging) {
+    var xx = x - this.context.canvas.offsetLeft - this.state.offsetX;
+    var yy = y - this.context.canvas.offsetTop - this.state.offsetY;
+    var p = points.add(xx, yy, dragging);
+
+    // Send points to server
+    //room.sendPoints(p.data());
+    // Start drawing
+    points.last(this._drawLine);
+  },
+
+  _startDrawing: function (e) {
+    this.setState({ painting: true });
+		this._addPoint(e.pageX, e.pageY);
+	},
+
+  _stopDrawing: function () {
+    this.setState({ painting: false });
+	},
+
+  _drawing: function (e) {
+		if (this.state.painting) {
+			this._addPoint(e.pageX, e.pageY, true);
+		}
+	},
+
+  // former draw()
+  _drawLine: function (prev, curr) {
+		if (typeof prev === 'undefined' || prev.color !== curr.color || prev.size !== curr.size) {
+			// Pencil style
+			this.context.strokeStyle = curr.color;
+			this.context.lineWidth = curr.size;
+		}
+
+		// Drawing
+		this.context.beginPath();
+
+		if (curr.dragging) {
+			this.context.moveTo(prev.x, prev.y);
+		} else {
+			this.context.moveTo(curr.x - 0.1, curr.y - 0.1); // Make a dot
+		}
+		this.context.lineTo(curr.x, curr.y);
+
+		this.context.closePath();
+		this.context.stroke();
+	},
+
   render: function () {
     return (
         <div id="artboard-wrapper">
-          <canvas id="artboard" width="800" height="600"></canvas>
+          <canvas id="artboard" width="800" height="600" ref="canvas"></canvas>
         </div>
         );
   }
