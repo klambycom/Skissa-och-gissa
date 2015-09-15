74890d48136ea2446abbf8a01f9e2e0d24d52614
klambycom <christian@klamby.com>
2015-09-15 12:08:07 +0200

    Validate layout props

--- src/components/app.js
+++ src/components/app.js
@@ -11,9 +11,24 @@ module.exports = React.createClass({
     return /\/game\//.test(this.context.router.getCurrentPathname());
   },
 
+  _cssPaths: function () {
+    return [this.props.css_path, this.props.fontAwesome_path];
+  },
+
+  _jsPaths: function () {
+    return [this.props.js_path];
+  },
+
   render: function () {
     return (
-        <Layout {...this.props} inGame={this._testPath()}>
+        <Layout
+          {...this.props}
+          inGame={this._testPath()}
+          title={this.props.page_title}
+          description={this.props.page_description}
+          css={this._cssPaths()}
+          js={this._jsPaths()}>
+
           <Flash messages={this.props.message} />
           <RouteHandler {...this.props} />
         </Layout>
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -4,23 +4,28 @@ var Popup = require('../popup');
 var SiteInformation = require('./site_information');
 
 module.exports = React.createClass({
+  propTypes: {
+    inGame: React.PropTypes.bool.isRequired,
+    title: React.PropTypes.string.isRequired,
+    description: React.PropTypes.string.isRequired,
+    css: React.PropTypes.array.isRequired,
+    js: React.PropTypes.array.isRequired
+  },
+
   _inGameClass: function (name) { return this.props.inGame ? name : ''; },
 
   render: function () {
-    // TODO <body className="game"> .game should be added later for effect! But
-    // should maybe remove later.
     return (
         <html lang="sv">
           <head>
             <meta charSet="utf-8" />
-            <title>{this.props.page_title}</title>
-            <link rel="stylesheet" href={this.props.css_path} />
-            <link rel="stylesheet" href={this.props.fontAwesome_path}/>
+            <title>{this.props.title}</title>
+            {this.props.css.map(function (path, i) { return <link rel="stylesheet" href={path} key={i} />; })}
           </head>
           <body className={this._inGameClass('game')}>
             <header id="header">
               <div>
-                <div id="header-title"><Link to="index">{this.props.page_title}</Link></div>
+                <div id="header-title"><Link to="index">{this.props.title}</Link></div>
                 <nav id="header-nav">
                   <ul>
                     <li><Link to="index">Förstasidan</Link></li>
@@ -30,7 +35,7 @@ module.exports = React.createClass({
               </div>
             </header>
 
-            <SiteInformation show={!this.props.inGame} text={this.props.page_description} />
+            <SiteInformation show={!this.props.inGame} text={this.props.description} />
 
             <main id="content" className={this._inGameClass('in-game')}>
               {this.props.children}
@@ -39,7 +44,8 @@ module.exports = React.createClass({
             <footer id="footer">Webbplatsen är skapad av <a href="http://christiann.se">Christian Nilsson</a> och koden finns på <a href="https://github.com/klambycom/Skissa-och-gissa/">Github</a>.</footer>
 
             <Popup />
-            <script src={this.props.js_path}></script>
+
+            {this.props.js.map(function (path, i) { return <script src={path} key={i}></script>; })}
           </body>
         </html>
         );
