b55fd63c3d4f99ecddfce4199fa6b3280a8835ac
klambycom <christian@klamby.com>
2015-09-21 12:55:56 +0200

    Move user logic to mixin

---
+++ docs/js/components/mixins/user_mixin.markdown
@@ -0,0 +1,8 @@
+
+
+<!-- Start src/components/mixins/user_mixin.js -->
+
+## React
+
+<!-- End src/components/mixins/user_mixin.js -->
+
--- src/components/layout/user_login.js
+++ src/components/layout/user_login.js
@@ -1,32 +1,19 @@
 var React = require('react');
-var Reflux = require('reflux');
 var Link = require('react-router').Link;
 var ProfilePicture = require('../profile_picture');
 var Lobby = require('../../browser/lobby');
+var UserMixin = require('../mixins/user_mixin');
 
 module.exports = React.createClass({
   displayName: 'UserLogin',
 
-  mixins: [
-    Reflux.connectFilter(Lobby.store, 'user', function (data) {
-      if (data.event === 'user' && data.type === 'login') { return data.data; }
-      return this.state.user;
-    }.bind(this))
-  ],
-
-  propTypes: {
-    user: React.PropTypes.object.isRequired
-  },
+  mixins: [UserMixin],
 
   getInitialState: function () {
-    return { user: {}, showMore: false };
+    return { showMore: false };
   },
 
   componentDidMount: function () {
-    if (typeof this.props.user !== 'undefined') {
-      Lobby.user.login(this.props.user);
-    }
-
     document.addEventListener('click', this._showLess);
   },
 
@@ -41,10 +28,8 @@ module.exports = React.createClass({
     this.setState({ showMore: false });
   },
 
-  _signedIn: function () { return !!this.state.user.provider; },
-
   render: function () {
-    if (!this._signedIn()) {
+    if (!this.isSignedIn()) {
       return (
           <div id="user-login">
             <a href="/login/facebook">Logga in med Facebook</a>
@@ -52,13 +37,11 @@ module.exports = React.createClass({
           );
     }
 
-    console.log('USER', this.state.user);
-
     return (
         <div id="user-login" className="facebook">
           <a href="#" onClick={this.handleShowMore}>
-            <ProfilePicture user={this.state.user} />
-            {this.state.user.firstName}
+            <ProfilePicture user={this.user} />
+            {this.user.firstName}
           </a>
 
           <div className="more" style={this.state.showMore ? {} : { display: 'none' }}>
