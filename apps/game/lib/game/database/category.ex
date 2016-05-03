edfc03be3532c00dca958f5e4b851cba1a5c17ed
klambycom <christian@klamby.com>
2016-05-03 22:21:15 +0200

    Get category and words

--- apps/game/lib/game/database/category.ex
+++ apps/game/lib/game/database/category.ex
@@ -1,11 +1,19 @@
 defmodule Game.Database.Category do
   use Ecto.Schema
 
+  import Ecto.Query, only: [from: 2]
+
   schema "categories" do
     field :name, :string
     field :description, :string
     has_many :words, Game.Database.Word
 
     timestamps
+  end
+
+  # TODO Only categories with more than some words
+  def with_words do
+    from p in Game.Database.Category,
+      preload: [:words]
   end
 end
