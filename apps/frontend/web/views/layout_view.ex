11807c32782b5bdc794c826dce743e7f606d7761
klambycom <christian@klamby.com>
2016-05-22 15:57:08 +0200

    Style flash messages

--- apps/frontend/web/views/layout_view.ex
+++ apps/frontend/web/views/layout_view.ex
@@ -5,4 +5,17 @@ defmodule Frontend.LayoutView do
   Returns current locale.
   """
   def locale, do: Gettext.get_locale(Frontend.Gettext)
+
+  @doc """
+  Get flash element as p-tag.
+
+  ## Example
+
+      iex> flash_element conn, :info, class: "alert" # => <p class="alert">Msg</p>
+  """
+  def flash_element(conn, key, opts \\ %{}) do
+    if get_flash(conn, key) != nil do
+      content_tag :p, get_flash(conn, key), opts
+    end
+  end
 end
