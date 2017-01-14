504c92b7971a5d372617069487e5153992ff4d87
Christian Nilsson <christian@klamby.com>
2017-01-14 20:33:11 +0100

    Set up React

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: "babel",
+      loader: "babel-loader",
       query: {
-        presets: ["es2015"]
+        presets: ["es2015", "react"]
       }
     }]
   },
