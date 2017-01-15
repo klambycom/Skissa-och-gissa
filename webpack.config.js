2e25830909c31712176fb299723171133dfbb5ed
klambycom <christian@klamby.com>
2017-01-15 22:45:04 +0100

    Set up less

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,45 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+
 module.exports = {
-  entry: "./web/static/js/app.js",
+  devtool: "source-map",
+  entry: {
+    app: "./web/static/js/app.js",
+    game: "./web/static/js/game.js",
+    admin: "./web/static/js/admin.js"
+  },
   output: {
-    path: "./priv/static/js",
-    filename: "app.js"
+    path: "./priv/static",
+    filename: "js/[name].js"
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel-loader",
-      query: {
-        presets: ["es2015", "react"]
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015", "react"]
+        }
+      },
+      {
+        test: /\.less/,
+        loader: ExtractTextPlugin.extract(
+          "css?sourceMap!" +
+          "less?sourceMap"
+        )
       }
-    }]
+    ]
   },
   resolve: {
-    modulesDirectories: ["node_modules"]
-  }
+    modulesDirectories: [
+      "node_modules",
+      __dirname + "/web/static/js",
+      __dirname + "/web/static/css"
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css"),
+    new CopyWebpackPlugin([{from: "./web/static/assets"}])
+  ]
 };
