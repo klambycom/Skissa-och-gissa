932b555eeff6a64cdef84fbbd171ab8b99b9f019
klambycom <christian@klamby.com>
2017-01-15 16:54:31 +0100

    Load css and js depending on page

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,9 @@
 defmodule SkissaOchGissa.LayoutView do
   use SkissaOchGissa.Web, :view
+
+  def static_file(%{path_info: ["admin" | _]}, type), do: get_path("admin", type)
+  def static_file(%{path_info: ["games" | _]}, type), do: get_path("game", type)
+  def static_file(_conn, type), do: get_path("app", type)
+
+  defp get_path(file, type), do: "/#{Atom.to_string(type)}/#{file}.#{Atom.to_string(type)}"
 end
