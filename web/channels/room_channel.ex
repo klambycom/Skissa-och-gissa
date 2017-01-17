6b084f191c2ef96e5fb744260d90b9d4d0972d7c
klambycom <christian@klamby.com>
2017-01-17 20:34:32 +0100

    Check if game exists before joining

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -2,13 +2,19 @@ defmodule SkissaOchGissa.RoomChannel do
   use SkissaOchGissa.Web, :channel
 
   alias SkissaOchGissa.Presence
+  alias SkissaOchGissa.Game
 
-  def join("room:lobby", _params, socket) do
-    send self(), :after_join
-    {:ok, socket}
+  def join("room:" <> id, _params, socket) do
+    case Repo.get(Game, id) do
+      nil ->
+        {:error, "Room does not exist."}
+      game ->
+        send self(), :after_join
+        {:ok, assign(socket, :game, game)}
+    end
   end
 
-  def join(_other, _params, socket) do
+  def join(_other, _params, _socket) do
     {:error, "Room does not exist."}
   end
 
