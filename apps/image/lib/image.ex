f8b1210af8ee9810b533d0b94998bca721cba0b2
klambycom <christian@klamby.com>
2017-05-28 21:55:31 +0200

    Initial Image commit

---
+++ apps/image/lib/image.ex
@@ -0,0 +1,18 @@
+defmodule Image do
+  @moduledoc """
+  Documentation for Image.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Image.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
