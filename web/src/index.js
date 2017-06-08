edb66b1c704d35f2bda82843236b6b7f79f13e47
klambycom <christian@klamby.com>
2017-06-08 23:27:41 +0200

    Setup sass

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import block from "bem-cn";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+
+block.setup({
+  el: "__",
+  mod: "--",
+  modValue: "-"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
