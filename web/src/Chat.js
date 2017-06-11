c2f526b959202e50660660c5d47141293010c287
klambycom <christian@klamby.com>
2017-06-11 22:16:35 +0200

    Start on chat

---
+++ web/src/Chat.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import bem from "bem-cn";
+
+import Input from "./Input";
+
+const b = bem("Chat");
+
+function Chat(props) {
+  return (
+    <div className={b}>
+      <div className={b("messages")}>
+      {props.messages.map((message, i) => <div key={i}>{message.body}</div>)}
+      </div>
+
+      <Input onEnter={(e) => props.onMessage(e)} />
+    </div>
+  );
+}
+
+Chat.defaultProps = {
+  messages: []
+};
+
+Chat.propTypes = {
+  onMessage: PropTypes.func.isRequired,
+  messages: PropTypes.array
+};
+
+export default Chat;
---
+++ web/src/Input.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ""};
+  }
+
+  handleKeyPress(e) {
+    if (this.props.onEnter && e.key === "Enter") {
+      this.props.onEnter(this.state.value);
+      this.setState({value: ""});
+    }
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        value={this.state.value}
+        onChange={(e) => this.setState({value: e.target.value})}
+        onKeyPress={(e) => this.handleKeyPress(e)}
+      />
+    );
+  }
+}
+
+Input.propTypes = {
+  onEnter: PropTypes.func
+};
+
+export default Input;
