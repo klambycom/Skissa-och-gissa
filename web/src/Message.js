1d4f1b4cf94ba611bc0256363ba712bf95785952
klambycom <christian@klamby.com>
2017-06-14 21:53:21 +0200

    Style the chat and messages

--- web/src/Message.js
+++ web/src/Message.js
@@ -1,19 +1,24 @@
 import React from "react";
+import bem from "bem-cn";
 
 import UserList from "./UserList";
 
+import "./Message.css";
+
+const b = bem("Message");
+
 function Text(props) {
   return (
-    <div>
-      {props.body}
-      <span>{props.user}</span>
+    <div className={b("Text")}>
+      <div className={b("Text", "body")}>{props.body}</div>
+      <div className={b("Text", "user")}>{props.user}</div>
     </div>
   );
 }
 
 function Users(props) {
   return (
-    <div>
+    <div className={b("Users")}>
       <div>{props.body}</div>
       <div>
         <UserList users={props.users} />
@@ -23,7 +28,11 @@ function Users(props) {
 }
 
 function MissingCommand(props) {
-  return (<div>The command '{props.command}' was not recognised.</div>);
+  return (
+    <div className={b("MissingCommand")}>
+      The command '{props.command}' was not recognised.
+    </div>
+  );
 }
 
 export default {Text, Users, MissingCommand};
